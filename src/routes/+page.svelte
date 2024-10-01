a2fcff1d768719ce2bc5216d2adbae49076e33a4
headmad <kov.ps@ya.ru>
2024-10-02 04:04:22 +0800

    another fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,15 +10,15 @@
   async function openScanner() {
     // if (qrScanner.open.isSupported()) {
       // Getting the first captured QR.
-      qrScanner.isOpened(); // false
-      let promise = qrScanner.open({ text: "Scan any QR" });
-      qrScanner.isOpened(); // true
-      await promise;
-      qrScanner.isOpened(); // false
+      // qrScanner.isOpened(); // false
+      // let promise = qrScanner.open({ text: "Scan any QR" });
+      // qrScanner.isOpened(); // true
+      // await promise;
+      // qrScanner.isOpened(); // false
 
       // Getting some specific QR.
       qrScanner.isOpened(); // false
-      promise = qrScanner.open({
+     let  promise = qrScanner.open({
         text: "Scan some specific QR",
         capture(qr) {
           result = qr;
