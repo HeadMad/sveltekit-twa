1fc25f630f447083169fd9052dbc948c0cdfc306
headmad <kov.ps@ya.ru>
2024-10-02 03:59:31 +0800

    scaner promise style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,22 +3,33 @@
 
   // import { retrieveLaunchParams } from '@telegram-apps/sdk';
 
-// const { initDataRaw } = retrieveLaunchParams();
+  // const { initDataRaw } = retrieveLaunchParams();
 
-  var result = '';
+  var result = "";
 
   async function openScanner() {
-      const promise = qrScanner.open({
-        text: "Сканируй это",
-        onCaptured(qr) {
-            qrScanner.close();
-            result = qr;
+    if (qrScanner.open.isSupported()) {
+      // Getting the first captured QR.
+      qrScanner.isOpened(); // false
+      let promise = qrScanner.open({ text: "Scan any QR" });
+      qrScanner.isOpened(); // true
+      await promise;
+      qrScanner.isOpened(); // false
+
+      // Getting some specific QR.
+      qrScanner.isOpened(); // false
+      promise = qrScanner.open({
+        text: "Scan some specific QR",
+        capture(qr) {
+          result = qr;
+          return qr === "some-specific-qr";
         },
       });
       qrScanner.isOpened(); // true
       await promise;
       qrScanner.isOpened(); // false
     }
+  }
 </script>
 
 <!-- {initDataRaw} -->
@@ -26,8 +37,5 @@
 <h1>Scanner</h1>
 ScannerResult: {result}
 
-<br>
+<br />
 <button on:click={openScanner}>Open Scanner</button>
-
-
-
