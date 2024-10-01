c5e151c5fccc971ce9b8bb829c1187be26f4c0d8
headmad <kov.ps@ya.ru>
2024-10-02 03:49:38 +0800

    scanner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,33 @@
 <script>
-  // import { qrScanner } from "@telegram-apps/sdk";
+  import { qrScanner } from "@telegram-apps/sdk";
 
-  import { retrieveLaunchParams } from '@telegram-apps/sdk';
+  // import { retrieveLaunchParams } from '@telegram-apps/sdk';
 
-const { initDataRaw } = retrieveLaunchParams();
+// const { initDataRaw } = retrieveLaunchParams();
 
   var result = '';
 
-  // async function openScanner() {
-  //     const promise = qrScanner.open({
-  //       text: "Scan the QR",
-  //       onCaptured(qr) {
-  //           qrScanner.close();
-  //           result = qr;
-  //       },
-  //     });
-      // qrScanner.isOpened(); // true
-      // await promise;
-      // qrScanner.isOpened(); // false
-    // }
+  async function openScanner() {
+      const promise = qrScanner.open({
+        text: "Scan the QR",
+        onCaptured(qr) {
+            qrScanner.close();
+            result = qr;
+        },
+      });
+      qrScanner.isOpened(); // true
+      await promise;
+      qrScanner.isOpened(); // false
+    }
 </script>
 
-{initDataRaw}
+<!-- {initDataRaw} -->
 
-<!-- <h1>Scanner</h1>
+<h1>Scanner</h1>
 ScannerResult: {result}
-{#if qrScanner.isSupported()}
+
 <br>
 <button on:click={openScanner}>Open Scanner</button>
-{/if} -->
 
 
 
