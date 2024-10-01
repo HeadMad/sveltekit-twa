71abcbba1ccbfc20343f2768f99e0242c3435db7
headmad <kov.ps@ya.ru>
2024-10-02 03:44:05 +0800

    init data raw

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,34 @@
 <script>
-  import { qrScanner } from "@telegram-apps/sdk";
+  // import { qrScanner } from "@telegram-apps/sdk";
+
+  import { retrieveLaunchParams } from '@telegram-apps/sdk';
+
+const { initDataRaw } = retrieveLaunchParams();
 
   var result = '';
 
-  async function openScanner() {
-      const promise = qrScanner.open({
-        text: "Scan the QR",
-        onCaptured(qr) {
-            qrScanner.close();
-            result = qr;
-        },
-      });
+  // async function openScanner() {
+  //     const promise = qrScanner.open({
+  //       text: "Scan the QR",
+  //       onCaptured(qr) {
+  //           qrScanner.close();
+  //           result = qr;
+  //       },
+  //     });
       // qrScanner.isOpened(); // true
       // await promise;
       // qrScanner.isOpened(); // false
-    }
+    // }
 </script>
 
-<h1>Scanner</h1>
+{initDataRaw}
+
+<!-- <h1>Scanner</h1>
 ScannerResult: {result}
 {#if qrScanner.isSupported()}
 <br>
 <button on:click={openScanner}>Open Scanner</button>
-{/if}
+{/if} -->
 
 
 
