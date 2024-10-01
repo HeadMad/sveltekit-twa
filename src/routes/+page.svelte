c85c8c3cef332f76ce8ae63896dc74efd844eaa3
headmad <kov.ps@ya.ru>
2024-10-02 03:22:51 +0800

    scanner commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { qrScanner } from "@telegram-apps/sdk";
+
+  var result = '';
+
+  async function openScanner() {
+      const promise = qrScanner.open({
+        text: "Scan the QR",
+        onCaptured(qr) {
+            qrScanner.close();
+            result = qr;
+        },
+      });
+      // qrScanner.isOpened(); // true
+      // await promise;
+      // qrScanner.isOpened(); // false
+    }
+</script>
+
+<h1>Scanner</h1>
+ScannerResult: {result}
+{#if qrScanner.isSupported()}
+<br>
+<button on:click={openScanner}>Open Scanner</button>
+{/if}
+
+
+
