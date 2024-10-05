f05e09c522b5776e7d2caef2f96bc8ee40cd5fc1
HeadMad <kov.ps@ya.ru>
2024-10-06 00:40:48 +0800

    try hash

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,6 @@
-import {validateData} from '$lib';
+import { validateData } from '$lib';
 
-export async function load({fetch, url}) {
+export async function load({ fetch, url }) {
 
 
 
@@ -11,14 +11,18 @@ export async function load({fetch, url}) {
 
 export const actions = {
 
-  check: async ({request}) => {
-    console.log('CHECK')
-    const data = await request.formData();
-    const token = process.env.BOT_TOKEN;
-    const rawData = data.get('data');
-    const result = validateData(token, rawData);
-    if (!result)
-      return {ok: false, message: 'Invalid hash'};
-    return {ok: true, result};
+  check: async ({ request }) => {
+    try {
+      const data = await request.formData();
+      const token = process.env.BOT_TOKEN;
+      const rawData = data.get('data');
+      const result = validateData(token, rawData);
+      if (!result)
+        return { ok: false, message: 'Invalid hash'};
+      return { ok: true, result };
+    } catch (err) {
+      return {ok: false, message: err.message}
+    }
+
   }
 };
